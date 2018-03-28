1f2e2b87d59611d9e168f5135b89c9b820192de7
Omer Hefets <omerh127@gmail.com>
2018-03-29 00:33:00 +0300

    continue the PercStats

--- PercolationStats.java
+++ PercolationStats.java
@@ -3,6 +3,40 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class PercolationStats {
+    
+    double trial_arr[];
+    int size;
+    
     public PercolationStats(int n, int trials){
+        if (n <= 0 || trials <= 0){
+            throw new IllegalArgumentException();
+        }
+        
+        this.trial_arr = new double[trials];
+        this.size = n;
+        
+        for (int i = 0; i < trials; i++){
+            Percolation grid = new Percolation(n);
+            while (grid.percolates() == false){
+                int x = StdRandom.uniform(1,size+1);
+                int y = StdRandom.uniform(1,size+1);
+                if (grid.isFull(x,y) == true){
+                    grid.open(x,y);
+                }
+            }
+            trial_arr[i] = ((double) grid.open / (size * size));
+        }
     }
+    
+    public double mean(){
+        return StdStats.mean(trial_arr);
+    }
+    
+    public double stddev(){
+        return StdStats.stddev(trial_arr);
+    }
+    
+    public static void main(String[] args){
+        PercolationStats tri = new PercolationStats(10,10);
+   }
 }
