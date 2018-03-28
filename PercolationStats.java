dd3e08d578bf967293ec861791af1c2cbf564962
Omer Hefets <omerh127@gmail.com>
2018-03-28 23:36:59 +0300

    started PercStats file

---
+++ PercolationStats.java
@@ -0,0 +1,8 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class PercolationStats {
+    public PercolationStats(int n, int trials){
+    }
+}
